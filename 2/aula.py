40ce10470ace4950483067dcad0323bfa0127f9c
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-03 15:09:17 -0300

    aula 2

---
+++ 2/aula.py
@@ -0,0 +1,21 @@
+print('Informe o primeiro valor: ')
+firstValue = int(input())
+print('Informe o segundo valor: ')
+secondValue = int(input())
+response = firstValue + secondValue
+print('Soma = ', response)
+
+""" 
+Expressões Aritméticas
+
+Raiz quadrada √x: math.sqrt(x)
+Potenciação x²: math.pow(x,2)
+Módulo |x| : math.fabs(x)
+Fatorial x!: math.factorial(x)
+Maior divisor comum: math.gdc(x,y)
+Arredonda para o próximo inteiro: math.ceil(x)
+Trunca para inteiro: math.trunch(x)
+Funções trigonométricas: math.sin(x), math.cos(x), ...
+Constante PI π: math.pi
+
+"""

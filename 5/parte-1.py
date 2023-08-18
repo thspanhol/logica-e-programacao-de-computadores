c1bdaeece6e8ee81b6e759638fef948e4536905d
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-17 21:45:09 -0300

    aula 5

---
+++ 5/parte-1.py
@@ -0,0 +1,19 @@
+import math
+
+#for cont in range(10) : print(cont)
+#for cont in range(1, 12, 2) : print(cont)
+#for test in range(10, 0, -1) : print(test)
+
+#for num in range(1,20):
+#    print(f'{num:3}: {math.sqrt(num):.2f}')
+
+for cont in range(10):
+    if cont > 7:
+        break
+    print(cont)
+
+#for cont in range(10):
+#    if cont % 2 == 1:
+#        continue
+#    print(cont)
+

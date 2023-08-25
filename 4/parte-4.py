4215822f80e8d1608d9300e0e8ac94b9d1e77d24
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-25 20:37:01 -0300

    aula 4

--- 4/parte-4.py
+++ 4/parte-4.py
@@ -3,3 +3,9 @@ import random
 number = random.randint(1,7)
 string = random.choice(['Jo', 'Ken', 'Pô'])
 print(number, '-', string)
+
+def fun(param):
+    print(param)
+
+fun('Thales')
+fun('Spanhol')
